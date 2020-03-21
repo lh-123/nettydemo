b7c9cc3db0f1974fabd261ba813e95e44c1d5cec
lh-123 <1840561548@qq.com>
2020-03-21 21:51:10 +0800

    netty

---
+++ src/main/java/nettysock/nettydemo/websocket/MyServer.java
@@ -0,0 +1,61 @@
+package nettysock.nettydemo.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class MyServer {
+    public static void main(String[] args) throws Exception{
+
+
+        //创建两个线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(); //8个NioEventLoop
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workerGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ChannelPipeline pipeline = ch.pipeline();
+
+                    //因为基于http协议，使用http的编码和解码器
+                    pipeline.addLast(new HttpServerCodec());
+                    //是以块方式写，添加ChunkedWriteHandler处理器
+                    pipeline.addLast(new ChunkedWriteHandler());
+
+                    pipeline.addLast(new HttpObjectAggregator(8192));
+
+                    pipeline.addLast(new WebSocketServerProtocolHandler("/hello2"));
+
+                    //自定义的handler ，处理业务逻辑
+                    pipeline.addLast(new MyTextWebSocketFrameHandler());
+                }
+            });
+
+            //启动服务器
+            ChannelFuture channelFuture = serverBootstrap.bind(7000).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
